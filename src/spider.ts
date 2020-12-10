a58889c32c2d51bf9c5b2a2ebe1e64f5729fd077
deft <deftmail@163.com>
2020-12-10 12:56:48 +0800

    sohu temp

--- src/spider.ts
+++ src/spider.ts
@@ -5,19 +5,26 @@ class Spider {
   constructor() {
     this.getRawHtml()
   }
-  private url = `http://www.dell-lee.com/`.trim()
+  private url = `http://top.sogou.com/hot/shishi_1.html?fr=tph_righ/`.trim()
   async getRawHtml() {
     const ret = await superagent.get(this.url)
     this.getHotSearchInfo(ret.text)
   }
   async getHotSearchInfo(html: string) {
     const $ = cheerio.load(html)
-    const rank = $('.course-item')
-    rank.map((index, el) => {
-      console.log(index)
-      const txt = $(el).text()
-      console.log(txt)
+    const arr: string[] = []
+    const rank1 = $('.pub-list .p2')
+    rank1.map((index, el) => {
+      let txt = $(el).text().trim()
+      txt = txt.split('...')[0] + '...'
+      arr.push(txt)
     })
+    const rank2 = $('.pub-list .p3')
+    rank2.map((index, el) => {
+      const txt = $(el).text().trim()
+      arr.push(txt)
+    })
+    console.log(arr.join('\n')) // df-log
   }
 }
 
